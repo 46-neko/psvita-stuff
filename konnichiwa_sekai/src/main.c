52f94c6c56f9f3c06aeaeea1c39331db858939f4
k1tsunee <pedrobene13@gmail.com>
2021-04-05 19:23:28 -0300

    Added a simple ''Hello World''program. It's not displaying Japanese text, tho. I'll look at this later.

---
+++ konnichiwa_sekai/src/main.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<psp2/kernel/processmgr.h>
+#include<psp2/kernel/threadmgr.h>
+#include<psp2/ctrl.h>
+#include "/usr/local/vitasdk/share/gcc-arm-vita-eabi/samples/common/debugScreen.h"
+
+int main(int argc, char *argv[]){
+    psvDebugScreenInit(); // Initiates Debug Screen
+    psvDebugScreenPrintf("こんにちは世界!\n"); // Write something
+    psvDebugScreenPrintf("Made by k1tsunee\n"); // lol
+    psvDebugScreenPrintf("Press O to quit.\n");
+
+    SceCtrlData ctrl;
+    do{
+        sceCtrlPeekBufferPositive(0, &ctrl, 1);
+    }while(ctrl.buttons != SCE_CTRL_CIRCLE);
+    
+    sceKernelExitProcess(0); // Exit with code 0 (success)
+    return 0;
+}
